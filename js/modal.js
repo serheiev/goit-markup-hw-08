567af6e1cf70069e651dc251544b9db1336118f6
Den Serheiev <69833095+serheiev@users.noreply.github.com>
2022-07-27 10:30:45 +0300

    Initial commit

---
+++ js/modal.js
@@ -0,0 +1,22 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    const: body = document.querySelector('body')
+  };
+
+
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    body.classList.toggle('no-scroll');
+  }
+
+
+})();
+
+
